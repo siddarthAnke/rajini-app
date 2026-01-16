9dbfd5ade226256f50463b989d8eaf304dcb3492
siddarthAnke <siddarthrajini2@gmail.com>
2026-01-16 23:37:21 +0530

    fixed root controller to return index.html

--- src/main/java/com/srg/demo/controller/RootController.java
+++ src/main/java/com/srg/demo/controller/RootController.java
@@ -2,10 +2,11 @@ package com.srg.demo.controller;
 
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class RootController {
 
     @GetMapping("/")
