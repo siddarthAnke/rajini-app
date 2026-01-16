87535dd89b34d7400825c963981fefc3c3cf7cda
siddarthAnke <siddarthrajini2@gmail.com>
2026-01-16 21:13:00 +0530

    Added root endpoint for render

---
+++ src/main/java/com/srg/demo/controller/RootController.java
@@ -0,0 +1,15 @@
+package com.srg.demo.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
